9fc0f4a743e65d639ee46b84a7402219b7d02d04
prettykool <79608594+prettykool@users.noreply.github.com>
2021-02-24 16:59:46 -0600

    Update hal.c
    2 much bloat 4 me (also had to test GitHubs text editor)

--- hal.c
+++ hal.c
@@ -4,14 +4,9 @@
 
 int main (){
     int answer;
-    bool Y;
-    bool N;
-
-    Y = true;
-    N = false;
-
+    
     printf ("Are you sure about this, %s? Y/N. | ", getenv("USER"));
-    scanf (" %c", &answer);
+    scanf ("%c", &answer);
 
     while (answer == 'Y'){
         printf("You know I can't do that, %s.\n", getenv("USER"));
